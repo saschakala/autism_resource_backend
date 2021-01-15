ff4d07906ad761e466605132866f1bfb7bbdecf6
Sascha Kala <mxsaschakala@gmail.com>
2021-01-15 13:57:42 -0500

    fixed associations on models and many_to_many books to tags relationship works in console

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,7 +1,12 @@
 class Tag < ApplicationRecord
+    has_many :article_tags
     has_many :articles, through: :article_tags
+    has_many :book_tags
     has_many :books, through: :book_tags
+    has_many :misc_tags
     has_many :miscs, through: :misc_tags
+    has_many :multimedium_tags
     has_many :multimedia, through: :multimedium_tags
+    has_many :social_media_account_tags
     has_many :social_media_accounts, through: :social_media_account_tags
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Tag.create(tag_name: "meme")
+
+Tag.create(tag_name: "personal")
+
+Tag.create(tag_name: "queer")
+
+Tag.create(tag_name: "research")
+
+Tag.create(tag_name: "comedy")
+
+Tag.create(tag_name: "autobiographical")
+
+Tag.create(tag_name: "poetry")
+
+Tag.create(tag_name: "novel")
+
+Tag.create(tag_name: "short_story")
+
+
