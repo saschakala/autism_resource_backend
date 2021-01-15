59ffaf77038f5b7336b0b89337f598e218a5963c
Sascha Kala <mxsaschakala@gmail.com>
2021-01-15 18:45:18 -0500

    added skeleton of article seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,6 +24,12 @@ Tag.create(tag_name: "novel")
 
 Tag.create(tag_name: "short_story")
 
+Tag.create(tag_name: "essay")
+
+Tag.create(tag_name: "not_explicitly_autistic")
+
+Tag.create(tag_name: "created_by_autistic")
+
 Book.create(
     title: "Spectrum Women", 
     author:"Barb Cook & Dr. Michelle Garnett", 
@@ -31,4 +37,11 @@ Book.create(
     img_url: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages-na.ssl-images-amazon.com%2Fimages%2FI%2F71jcxn78s%2BL.jpg&imgrefurl=https%3A%2F%2Fwww.amazon.com%2FSpectrum-Women-Walking-Beat-Autism%2Fdp%2F1785924346&tbnid=8ArqDipia5la_M&vet=12ahUKEwj-2dXzwp7uAhXLeDABHcDhCVUQMygAegUIARCiAQ..i&docid=dCC0cMZmxrnBjM&w=848&h=1250&q=spectrum%20women&ved=2ahUKEwj-2dXzwp7uAhXLeDABHcDhCVUQMygAegUIARCiAQ", 
     tag_ids: %w[2 6 9])
 
+# Article.create(
+#     title: "The Expectancies and Motivations for Heavy Episodic Drinking of Alcohol in Autistic Adults",
+#     author: ,
+#     url: ,
+#     excerpt: ,
+#     tag_ids: 
+# )
 
