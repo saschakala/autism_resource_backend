4e017d8d75e8566f261b66ee356d5e05ad59dc42
Sascha Kala <mxsaschakala@gmail.com>
2021-01-15 20:03:47 -0500

    added many to many multimedia creator relationship to models

--- app/models/multimedium.rb
+++ app/models/multimedium.rb
@@ -1,3 +1,6 @@
 class Multimedium < ApplicationRecord
+    has_many :multimedium_tags
     has_many :tags, through: :multimedium_tags
+    has_many :mm_creators
+    has_many :creators, through: :mm_creators
 end
