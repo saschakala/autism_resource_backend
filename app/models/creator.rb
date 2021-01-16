556db7f71130aaa301dbecc06e4ce420da460f99
Sascha Kala <mxsaschakala@gmail.com>
2021-01-15 20:00:26 -0500

    added many to many social media account creator relationship to models

--- app/models/creator.rb
+++ app/models/creator.rb
@@ -3,4 +3,6 @@ class Creator < ApplicationRecord
     has_many :books, through: :book_creators
     has_many :article_creators
     has_many :articles, through: :article_creators
+    has_many :sma_creators
+    has_many :social_media_accounts, through: :sma_creators
 end
--- app/models/social_media_account.rb
+++ app/models/social_media_account.rb
@@ -1,3 +1,6 @@
 class SocialMediaAccount < ApplicationRecord
+    has_many :social_media_account_tags
     has_many :tags, through: :social_media_account_tags
+    has_many :sma_creators
+    has_many :creators, through: :sma_creators
 end
