7390d212896a89aee1a285f37d8f700b9bc75e82
Sascha Kala <mxsaschakala@gmail.com>
2021-01-18 13:59:27 -0500

    added has_many relationships to source serializer

--- app/serializers/source_serializer.rb
+++ app/serializers/source_serializer.rb
@@ -1,4 +1,6 @@
 class SourceSerializer
   include FastJsonapi::ObjectSerializer
   attributes :source_type, :title, :platform, :handle, :genre, :url, :img_url, :description
+  has_many :creators
+  has_many :tags
 end
