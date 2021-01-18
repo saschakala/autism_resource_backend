112451aa3e303eaf727d0ce72cb5f5c1f97a5fe2
Sascha Kala <mxsaschakala@gmail.com>
2021-01-18 16:10:32 -0500

    added another book to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,9 +39,10 @@ Creator.create(
 )
 # 2
 Creator.create(
-    first_name:"Dr. Michelle",
+    first_name:"Michelle",
     last_name:"Garnett",
     autistic: false,
+    title: "Dr.",
     url: "https://mindsandhearts.net/dr-michelle-garnett/"
 )
 
@@ -50,6 +51,7 @@ Creator.create(
     first_name:"Camilla",
     last_name:"Pang",
     autistic: true,
+    title: "PhD",
     url: "https://www.camillapang.com/"
 )
 
@@ -57,9 +59,20 @@ Source.create(
     source_type: "Book",
     title: "Spectrum Women", 
     genre: "Autobiographical", 
+    url: "https://www.goodreads.com/book/show/38884913-spectrum-women",
     img_url: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages-na.ssl-images-amazon.com%2Fimages%2FI%2F71jcxn78s%2BL.jpg&imgrefurl=https%3A%2F%2Fwww.amazon.com%2FSpectrum-Women-Walking-Beat-Autism%2Fdp%2F1785924346&tbnid=8ArqDipia5la_M&vet=12ahUKEwj-2dXzwp7uAhXLeDABHcDhCVUQMygAegUIARCiAQ..i&docid=dCC0cMZmxrnBjM&w=848&h=1250&q=spectrum%20women&ved=2ahUKEwj-2dXzwp7uAhXLeDABHcDhCVUQMygAegUIARCiAQ", 
     tag_ids: %w[2 6 9],
     creator_ids: %w[1 2]
+)
+
+Source.create(
+    source_type: "Book",
+    title: "An Outsider's Guide to Humans", 
+    genre: "Science", 
+    url: "https://www.goodreads.com/book/show/52559342-an-outsider-s-guide-to-humans",
+    img_url: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages-na.ssl-images-amazon.com%2Fimages%2FI%2F518ylyJxQYL._SX329_BO1%2C204%2C203%2C200_.jpg&imgrefurl=https%3A%2F%2Fwww.amazon.com%2FOutsiders-Guide-Humans-Science-Taught%2Fdp%2F1984881639&tbnid=Ho3tfyMf8KchSM&vet=12ahUKEwj6paLgsabuAhUi8VMKHbruDywQMygAegQIARBw..i&docid=LWpw93-wq3iIVM&w=331&h=499&q=an%20outsiders%20guide%20to%20humans&ved=2ahUKEwj6paLgsabuAhUi8VMKHbruDywQMygAegQIARBw", 
+    tag_ids: %w[4 8 12],
+    creator_ids: %w[3]
 )
 
 
