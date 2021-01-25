d27fbad9b7a1892d79c4a253dd7455a162a5263d
Sascha Kala <mxsaschakala@gmail.com>
2021-01-25 00:12:22 -0500

    fixed book tags json data

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,12 +4,12 @@ class Tag < ApplicationRecord
 
     def self.books
         book_tag = Tag.find_by(tag_name: 'book')
-        tags = book_tag.sources.map {|source| source.tags}
-        tags.map.with_index {|tag, i| tag[i]}
+        book_tag.sources.map {|source| source.tags}.flatten
     end
 
 
 
 end
+
 
 
