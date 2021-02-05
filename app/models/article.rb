b0fac5047a8dfe006e29222400581382de8cf850
“saschakala” <mxsaschakala@gmail.com>
2021-02-04 22:19:17 -0500

    added find by tag scope to article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,5 +4,7 @@ class Article < ApplicationRecord
     has_many :article_creators
     has_many :creators, through: :article_creators
 
+    scope :by_tag, -> (name) {joins(:tags).where('tags.tag_name' => name)}
+
 
 end
