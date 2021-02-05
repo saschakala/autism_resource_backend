da35a5ad658259858fe5d18ac9551bd9536b8008
“saschakala” <mxsaschakala@gmail.com>
2021-02-04 22:04:46 -0500

    created article resource

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   
   
   
+  resources :articles
   namespace :api do
     namespace :v1 do
       resources :sources do
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_02_160449) do
+ActiveRecord::Schema.define(version: 2021_02_05_030115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "genre"
+    t.string "url"
+    t.string "img_url"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "book_creators", force: :cascade do |t|
     t.bigint "book_id"
