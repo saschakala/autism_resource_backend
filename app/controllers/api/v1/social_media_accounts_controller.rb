5feea923998009f3df495430235fd5a638686dd9
“saschakala” <mxsaschakala@gmail.com>
2021-02-07 18:18:45 -0800

    added tag scope to sma model and created index and show methods in sma controller

--- app/controllers/api/v1/social_media_accounts_controller.rb
+++ app/controllers/api/v1/social_media_accounts_controller.rb
@@ -1,2 +1,22 @@
 class Api::V1::SocialMediaAccountsController < ApplicationController
+
+    def index
+        render json: SocialMediaAccountSerializer.new(SocialMediaAccount.all)
+    end
+
+    def show
+        if params[:id] == "all"
+            render json: SocialMediaAccountSerializer.new(SocialMediaAccount.all)
+        else
+        sma_by_tag = SocialMediaAccount.by_tag(params[:id])
+        render json: SocialMediaAccountSerializer.new(sma_by_tag)
+        end
+    end
+
+    private
+
+    def social_media_account_params
+        params.require(:social_media_account).permit(:id)
+    end
+
 end
--- app/models/social_media_account.rb
+++ app/models/social_media_account.rb
@@ -4,5 +4,7 @@ class SocialMediaAccount < ApplicationRecord
 
     has_many :sma_creators
     has_many :creators, through: :sma_creators
+
+    scope :by_tag, -> (name) {joins(:tags).where('tags.tag_name' => name)}
  
 end
