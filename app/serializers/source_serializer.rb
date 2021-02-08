77cc3a7f11a5550e132145fdaafa4785a31b7031
“saschakala” <mxsaschakala@gmail.com>
2021-02-07 18:09:14 -0800

    deleted source serializer

--- app/serializers/source_serializer.rb
+++
@@ -1,9 +0,0 @@
-class SourceSerializer
-  include JSONAPI::Serializer
-  attributes :source_type, :title, :platform, :handle, :genre, :url, :img_url, :description, :creators, :tags
-  # has_many :creators
-  # has_many :tags
-end
-
-
-
