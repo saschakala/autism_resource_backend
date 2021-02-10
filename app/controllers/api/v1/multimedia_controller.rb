1947002ad803c2f32c2b67a19e55d272fac770d2
“saschakala” <mxsaschakala@gmail.com>
2021-02-09 21:13:22 -0800

    added index show and params to multimedium controller

--- app/controllers/api/v1/multimedia_controller.rb
+++ app/controllers/api/v1/multimedia_controller.rb
@@ -1,2 +1,22 @@
 class Api::V1::MultimediaController < ApplicationController
+
+    def index
+        render json: MultimediumSerializer.new(Multimedium.all)
+    end
+
+    def show
+        if params[:id] == "all"
+            render json: MultimediumSerializer.new(Multimedium.all)
+        else
+        mm_by_tag = MultimediumSerializer.by_tag(params[:id])
+        render json: MultimediumSerializer.new(mm_by_tag)
+        end
+    end
+
+    private
+
+    def multimedium_params
+        params.require(:multimedium).permit(:id)
+    end
+
 end
