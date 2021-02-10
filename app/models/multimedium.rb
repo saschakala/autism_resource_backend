d246ecd866101a6606b36dd9e8e7c8086e5b04de
“saschakala” <mxsaschakala@gmail.com>
2021-02-09 21:14:05 -0800

    added by_tag scope to multimedium models

--- app/models/multimedium.rb
+++ app/models/multimedium.rb
@@ -3,4 +3,6 @@ class Multimedium < ApplicationRecord
     has_many :tags, through: :multimedia_tags
     has_many :multimedia_creators
     has_many :creators, through: :multimedia_creators
+
+    scope :by_tag, -> (name) {joins(:tags).where('tags.tag_name' => name)}
 end
