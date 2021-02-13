029fe64f2f62ea04cd8ebc4707f9b56b1187104f
“saschakala” <mxsaschakala@gmail.com>
2021-02-12 20:37:57 -0800

    created index show method in film controller

--- app/controllers/api/v1/films_controller.rb
+++ app/controllers/api/v1/films_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::FilmsController < ApplicationController
+
+    def index
+        render json: FilmSerializer.new(Film.all)
+    end
+
+    # def show
+    #     if params[:id] == "all"
+    #         render json: FilmSerializer.new(Film.all)
+    #     else
+    #     films_by_tag = Film.by_tag(params[:id])
+    #     render json: FilmSerializer.new(films_by_tag)
+    #     end
+    # end
+
+    private
+
+    # def film_params
+    #     params.require(:film).permit(:id)
+    # end
+
+
 end
