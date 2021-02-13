75898665aa6b66999f36beeeafbb3633e6cb5e03
“saschakala” <mxsaschakala@gmail.com>
2021-02-12 20:39:47 -0800

    fixed syntax film serializer'

--- app/serializers/film_serializer.rb
+++ app/serializers/film_serializer.rb
@@ -1,4 +1,4 @@
 class FilmSerializer
   include JSONAPI::Serializer
-  attributes :title, :genre, :url, :img_url, :description :creators, :tags
+  attributes :title, :genre, :url, :img_url, :description, :creators, :tags
 end
